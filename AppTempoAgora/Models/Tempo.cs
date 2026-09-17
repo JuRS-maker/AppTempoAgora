349857ecaebbdb1bd038fcca3cf75f304625dbb4
JuRS-maker <js798853@gmail.com>
2026-09-17 18:24:16 -0300

    Criação do novo app TempoAgora. Foram colocados os APIs, criação das classes Models e DataService, um pouco de front-end e a maior parte das funcionalidades do app. é preciso fazer alguns ajustes, mas já está rodando.

---
+++ AppTempoAgora/Models/Tempo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace AppTempoAgora.Models
+{
+    public class Tempo
+    {
+        [JsonPropertyName("lon")]
+        public double? Lon { get; set; }
+
+        [JsonPropertyName("lat")]
+        public double? Lat { get; set; }
+
+        [JsonPropertyName("temp_min")]
+        public double? TempMin { get; set; }
+
+        [JsonPropertyName("temp_max")]
+        public double? TempMax { get; set; }
+
+        [JsonPropertyName("visibility")]
+        public int? Visibility { get; set; }
+
+        [JsonPropertyName("speed")]
+        public double? Speed { get; set; }
+
+        [JsonPropertyName("main")]
+        public string? Main { get; set; }
+
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+
+        [JsonPropertyName("sunrise")]
+        public string? Sunrise { get; set; }
+
+        [JsonPropertyName("sunset")]
+        public string? Sunset { get; set; }
+    }
+}
---
+++ AppTempoAgora/Services/DataService.cs
@@ -0,0 +1,53 @@
+﻿using AppTempoAgora.Models;
+using Newtonsoft.Json.Linq;
+using System.Data;
+
+namespace AppTempoAgora.Services
+{
+    internal class DataService
+    {
+        public static async Task<Tempo?>GetPrevisao(string cidade) 
+        {
+            Tempo? t = null;
+
+            string chave = "381f5ab8778b4901fc3e0dd07858c3ed";
+
+            string url = $"https://api.openweathermap.org/data/2.5/weather?" +
+                         $"q={cidade}&units=metric&appid={chave}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage resp = await client.GetAsync(url);
+
+                if (resp.IsSuccessStatusCode)
+                {
+                    string json = await resp.Content.ReadAsStringAsync();
+
+                    var rascunho = JObject.Parse(json);
+
+                    DateTime time = new();
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    t = new()
+                    {
+                        Lat = (double)rascunho["coord"]["lat"],
+                        Lon = (double)rascunho["coord"]["lon"],
+                        Description = (string)rascunho["weather"][0]["description"],
+                        Main = (string)rascunho["weather"][0]["main"],
+                        TempMin = (double)rascunho["main"]["temp_min"],
+                        TempMax = (double)rascunho["main"]["temp_max"],
+                        Speed = (double)rascunho["wind"]["speed"],
+                        Visibility = (int)rascunho["visibility"],
+                        Sunrise = sunrise.ToString(),
+                        Sunset = sunset.ToString(),
+                     }; //Fecha o objeto do tempo
+                }// Fecha if se o status do servidor foi de sucesso
+            }// Fecha laço using
+            
+
+
+            return t;
+        }
+    }
+}
