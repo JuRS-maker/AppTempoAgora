12574f1c22d8df27ae12309ef29975f42d90e9a1
JuRS-maker <js798853@gmail.com>
2026-09-24 20:13:37 -0300

    Melhorando a aplicação: mapa e mostrar localização atual (não funciona a chave da API).

--- AppTempoAgora/MainPage.xaml.cs
+++ AppTempoAgora/MainPage.xaml.cs
@@ -1,18 +1,18 @@
 ﻿using AppTempoAgora.Models;
 using AppTempoAgora.Services;
+using System.Diagnostics;
 
 namespace AppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+   
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked_Previsao(object sender, EventArgs e)
         {
             try
             {
@@ -35,6 +35,15 @@ namespace AppTempoAgora
                                          $"Descrição do clima: {t.Description} \n";
 
                         lbl_res.Text = dados_previsao;
+
+                        string mapa = $"https://embed.windy.com/embed.html?" +
+                                      $"type=map&location=coordinates&metricRain=mm&metricTemp=°C" +
+                                      $"&metricWind=km/h&zoom=5&overlay=wind&product=ecmwf&level=surface" +
+                                      $"&lat={t.Lat.ToString().Replace(",", ".")}&lon={t.Lon.ToString().Replace(",", ".")}";
+                        
+                        wv_mapa.Source = mapa;
+
+                        Debug.WriteLine(mapa);
                     }
                     else
                     {
@@ -52,5 +61,71 @@ namespace AppTempoAgora
                 await DisplayAlert("Ops", ex.Message, "OK");
             }
         }
+
+        private async void Button_Clicked_Localizacao(object sender, EventArgs e)
+        {
+            try 
+            {
+                GeolocationRequest request = 
+                    new GeolocationRequest
+                    (
+                        GeolocationAccuracy.Medium,
+                        TimeSpan.FromSeconds(10)
+                    );
+
+                Location? local = await Geolocation.Default.GetLocationAsync(request);
+                
+                if(local != null)
+                {
+                    string local_disp = $"Latitude: {local.Latitude} \n" +
+                                        $"Longitude: {local.Longitude} \n";
+
+                    lbl_coords.Text = local_disp;
+
+                    //Pega o nome da cidade que está na coordenada
+                    GetCidade(local.Latitude, local.Longitude);
+
+                } else
+                {
+                    lbl_coords.Text = "Nenhuma localização";
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Erro: Recurso não suportado no dispositivo.", fnsEx.Message, "OK");
+            }
+            catch (FeatureNotEnabledException fneEx) 
+            {
+                await DisplayAlert("Erro: Recurso não habilitado no dispositivo.", fneEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Erro:Recurso permitido no dispositivo.", pEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro!", ex.Message, "OK");
+            }
+        }
+
+        private async void GetCidade(double Lat, double Lon)
+        {
+            try
+            {
+                IEnumerable<Placemark> places = await Geocoding.Default.GetPlacemarksAsync(Lat, Lon);
+
+                Placemark? place = places.FirstOrDefault();
+
+                if (place != null)
+                {
+                    txt_cidade.Text = place.Locality;
+                }
+            } 
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro: Obtenção do nome da cidade", ex.Message, "OK");
+            }
+        }   
+
     }
 }
